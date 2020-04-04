6ee360965fd6372f27fd930348e73b12f47b9a53
Alexey Suppes <salxs86@gmail.com>
2020-04-04 08:19:56 +0500

    Фикс состояний ссылок

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+  return gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
@@ -17,21 +17,21 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.scss", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("sass/**/*.scss", gulp.series("css"));
+  gulp.watch("*.html").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
