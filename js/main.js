b712c15d0e63c030332b53780099b2dc17fe1bd7
Alexey Suppes <salxs86@gmail.com>
2020-04-04 14:10:36 +0500

    Добавлен показ попапа

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var pageUrl=location.search.substring(1);
 
 if (pageUrl === 'ip') {
@@ -7,3 +9,22 @@ if (pageUrl === 'ip') {
   var oooFormInput = document.querySelector('#bis-form-ooo');
   oooFormInput.checked = true;
 }
+
+var popupLinks = document.querySelectorAll('.js-popup-open');
+var popup = document.querySelector('.popup');
+var popupClose = document.querySelector('.popup__close');
+console.log(popupLinks.length);
+
+for (var i = 0; i < popupLinks.length; i++) {
+  var popupLink = popupLinks[i];
+  popupLink.addEventListener('click', function (event) {
+    event.preventDefault(event);
+    popup.classList.remove('visually-hidden');
+  });
+
+  popupClose.addEventListener('click', function (event) {
+    event.preventDefault(event);
+    popup.classList.add('visually-hidden');
+  })
+};
+
